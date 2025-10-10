35716916bfcb0c5e21a175ad32b84440a5b7e892
zayedQS <zayedqs1@gmail.com>
2025-10-10 12:31:55 +0300

    Add files via upload

---
+++ basics2.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+
+int main (){
+
+int x; 
+int y; 
+
+std::cout << "write value " << '\n'; 
+std::cin >> x;
+
+std:: cout << " write other value " << '\n';
+std::cin >> y; 
+
+
+std::cout << "sum :" << x * y; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0; 
+}
