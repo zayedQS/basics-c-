202c9b8317c61ea857bc2c8e2944afbe0c261bf7
zayedQS <zayedqs1@gmail.com>
2025-10-24 02:32:01 +0300

    Add files via upload

---
+++ triangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include  <cmath>
+using namespace std; 
+ 
+int main (){
+
+double x   ; 
+double  y ; 
+double z  ;
+
+x =pow(x , 2);
+cout << "enter a value " << '\n';
+ cin >> x;
+
+y =pow(y , 2);
+cout << "another value "<< '\n'; 
+
+cin >> y; 
+
+ z = x + y ;
+ z = sqrt(z);
+
+cout << z << '\n';
+
+
+    return 0;
+}
