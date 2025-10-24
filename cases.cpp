ce608cd19367dcc7fc8fb91834b52766c721aa06
zayedQS <zayedqs1@gmail.com>
2025-10-24 03:39:14 +0300

    Add files via upload

---
+++ cases.cpp
@@ -0,0 +1,62 @@
+#include <iostream> 
+
+using namespace std; 
+ 
+ int main (){
+int l;
+std::cout << "how much love: from (1-5) " << '\n'; 
+ std::cin >> l;
+
+ //switch is more efficience than if else
+ 
+ switch(l){
+case 1:
+cout << "hi" << '\n';
+break;
+case 2:
+cout << "hii" << '\n';
+break;
+case 3: 
+cout << "hiii" << '\n';
+break;
+case 4: 
+cout << "hiiii" << '\n';
+break; 
+case 5 : 
+cout << "hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii" << '\n';
+break;
+
+default: 
+cout << "enter a num from 1 - 5 (not more 😠)";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+    return 0; 
+ }
